11c4116df972d22421ef6f2a2e7f8ae91b605ceb
digitalvizyon <digitalvizyon@DESKTOP-7EN7IP7>
2024-09-16 11:24:39 +0300

    Add project files.

---
+++ AselsanCsharpEducation/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System;
+
+namespace AselsanCsharpEducation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Deger atanmayan degiskenler
+            int NumberAsEmpty;
+
+            //VALUE TYPES --> Tam Sayilar
+            int MyNumber = 10;
+            byte MyByte = 0x31;
+            short MyShort = 200;
+            long MyLong = 100;
+            
+            //VALUE TYPES --> Ondalikli Degerler
+            float MyFloat = 1.50f;
+            double numberDouble = 10.00;
+            decimal numberDecimal = 20.00m;
+            DateTime dateTime = DateTime.Now;
+
+            //Console Yazdirma Ornekleri
+            Console.WriteLine($"Gun: {dateTime.Day}, Ay: {dateTime.Month} Yıl: {dateTime.Year}");
+            Console.WriteLine($"Saat:{dateTime.TimeOfDay}");
+
+            //Place holder kullanimi
+            string rowProductName = "name:{0}";
+            Console.WriteLine(rowProductName, MyNumber);
+
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
